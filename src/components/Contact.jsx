eae964360a0d9f6379cc3c486cd997394123537b
mucahit <mucahitakdeniz28@gmail.com>
2024-05-25 21:43:40 +0300

    ContactLink

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Typography, Link } from "@mui/material";
 import React from "react";
 import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -12,7 +12,7 @@ const Contact = () => {
       <Typography variant="h4" color="#ba000d" display="flex" marginTop={2}>
         Contact
       </Typography>
-      <Card sx={{ width: "95%", boxShadow: 4,margin:"auto",p:1 }}>
+      <Card sx={{ width: "95%", boxShadow: 4, margin: "auto", p: 1 }}>
         <Typography variant="h6" display="flex" p={2}>
           Demo TrainIQ
         </Typography>
@@ -25,11 +25,23 @@ const Contact = () => {
             gap: 1,
           }}
         >
-          <FacebookTwoToneIcon sx={{ color: "info.main", fontSize: "2rem" }} />
-          <InstagramIcon sx={{ color: "error.main", fontSize: "2rem" }} />
-          <XIcon sx={{ color: "text.primary", fontSize: "1.8rem" }} />
-          <YouTubeIcon sx={{ color: "error.main", fontSize: "2.3rem" }} />
-          <LinkedInIcon sx={{ color: "primary.main", fontSize: "2rem" }} />
+          <Link target="_blank" href="https://tr-tr.facebook.com/">
+            <FacebookTwoToneIcon
+              sx={{ color: "info.main", fontSize: "2rem" }}
+            />
+          </Link>
+          <Link target="_blank" href="https://www.instagram.com/?hl=tr">
+            <InstagramIcon sx={{ color: "error.main", fontSize: "2rem" }} />
+          </Link>
+          <Link target="_blank" href="https://x.com/">
+            <XIcon sx={{ color: "text.primary", fontSize: "1.8rem" }} />
+          </Link>
+          <Link target="_blank" href="https://www.youtube.com/?ysclid=lwmggevncx319823064">
+            <YouTubeIcon sx={{ color: "error.main", fontSize: "2.3rem" }} />
+          </Link>
+          <Link target="_blank" href="https://www.linkedin.com/">
+            <LinkedInIcon sx={{ color: "primary.main", fontSize: "2rem" }} />
+          </Link>
         </CardContent>
         <CardContent
           sx={{
